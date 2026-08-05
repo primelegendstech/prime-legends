b58158790badb25b5dbb93a20c207a9d1eeb1007
Izigamex <primelegendsx@gmail.com>
2026-08-05 06:22:12 -0300

    Adiciona Header na página de ativação

--- app/ativacao/page.tsx
+++ app/ativacao/page.tsx
@@ -1,9 +1,13 @@
+import Header from "@/components/Header";
 import AtivacoesContent from "@/components/AtivacoesContent";
 
 export default function AtivacaoPage() {
   return (
     <main className="min-h-screen bg-black px-4 py-12">
-      <AtivacoesContent />
+      <Header />
+      <div className="pt-24">
+        <AtivacoesContent />
+      </div>
     </main>
   );
 }
